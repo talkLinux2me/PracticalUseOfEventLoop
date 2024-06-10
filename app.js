0b084a08fe5eab7e20854a63e67bd4d8c719d002
talkLinux2me <167791277+talkLinux2me@users.noreply.github.com>
2024-06-10 16:36:38 -0400

    counter

--- app.js
+++ app.js
@@ -4,3 +4,5 @@
 //Create a simple function that increments the variable, and then calls itself recursively.
 //Surround the initial function call in a try/catch block.
 //Within the catch, log the error and the value of the counter variable.
+
+let counter = 0;
