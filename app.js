458c9d7343c954444f7a11ab45ccc54a993a3027
talkLinux2me <167791277+talkLinux2me@users.noreply.github.com>
2024-06-10 16:34:52 -0400

    initializing project

---
+++ app.js
@@ -0,0 +1,6 @@
+
+//PART ONE
+//Declare a global counter variable.
+//Create a simple function that increments the variable, and then calls itself recursively.
+//Surround the initial function call in a try/catch block.
+//Within the catch, log the error and the value of the counter variable.
